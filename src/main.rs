4843c2bc3bfd19e38e284ec2c20640cc1849e18f
Felipe Garcia <felipefegar@gmail.com>
2020-12-16 11:44:38 -0300

    Added solution for day 15.

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ mod day11;
 mod day12;
 mod day13;
 mod day14;
+mod day15;
 mod utils;
 
 fn main() {
@@ -34,6 +35,7 @@ fn main() {
         12 => day12::run(),
         13 => day13::run(),
         14 => day14::run(),
+        15 => day15::run(),
         _ => println!("invalid day"),
     }
 }
