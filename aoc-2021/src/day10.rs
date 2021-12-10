3b66f656e082280be2c493ca687c5d44e4a0d8cb
Felipe Garcia <felipefegar@gmail.com>
2021-12-10 13:47:13 -0300

    Added solution for day 10.

---
+++ aoc-2021/src/day10.rs
@@ -0,0 +1,141 @@
+use aoc_utils;
+use itertools::Itertools;
+use rayon::prelude::*;
+
+#[derive(Debug, Clone)]
+struct Validation {
+    full: String,
+    status: Status,
+    error: usize,
+    stack: String,
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum Status {
+    Valid,
+    Incomplete,
+    Corrupted,
+}
+
+pub fn run() {
+    let lines: Vec<String> = aoc_utils::read_lines("inputs/day10.txt", true).collect();
+
+    let validations = check_lines(&lines);
+
+    let syntax_error = sum_syntax_error_score(&validations);
+    let autocomplete = median_autocomplete_score(&validations);
+
+    println!("{}", syntax_error);
+    println!("{}", autocomplete);
+}
+
+fn check_lines(lines: &[String]) -> Vec<Validation> {
+    lines.par_iter().map(|line| check_line(line)).collect()
+}
+
+fn check_line(line: &str) -> Validation {
+    let mut status: Status = Status::Valid;
+    let mut error: usize = 0;
+    let mut stack: Vec<char> = vec![];
+
+    for (index, symbol) in line.trim().chars().enumerate() {
+        match symbol {
+            '(' | '[' | '{' | '<' => stack.push(symbol),
+            ')' | ']' | '}' | '>' => match stack.last() {
+                None => {
+                    status = Status::Corrupted;
+                    error = index;
+                    break;
+                }
+                Some(&last) if invert(last) != symbol => {
+                    status = Status::Corrupted;
+                    error = index;
+                    break;
+                }
+                _ => {
+                    stack.pop();
+                }
+            },
+            _ => (),
+        }
+    }
+
+    if status == Status::Valid && !stack.is_empty() {
+        status = Status::Incomplete
+    }
+
+    Validation {
+        full: line.to_string(),
+        status,
+        error,
+        stack: stack.iter().join(""),
+    }
+}
+
+fn sum_syntax_error_score(validations: &[Validation]) -> usize {
+    validations
+        .iter()
+        .filter(|validation| validation.status == Status::Corrupted)
+        .map(|validation| syntax_error_score(validation))
+        .sum()
+}
+
+fn syntax_error_score(validation: &Validation) -> usize {
+    let error = validation
+        .full
+        .chars()
+        .nth(validation.error)
+        .expect("error");
+
+    match error {
+        ')' => 3,
+        ']' => 57,
+        '}' => 1197,
+        '>' => 25137,
+        _ => 0,
+    }
+}
+
+fn median_autocomplete_score(validations: &[Validation]) -> usize {
+    let scores: Vec<usize> = validations
+        .iter()
+        .filter(|validation| validation.status == Status::Incomplete)
+        .map(|validation| autocomplete_score(validation))
+        .sorted()
+        .collect();
+
+    scores[scores.len() / 2]
+}
+
+fn autocomplete_score(validation: &Validation) -> usize {
+    validation
+        .stack
+        .chars()
+        .map(|symbol| invert(symbol))
+        .rev()
+        .fold(0, |acc, symbol| {
+            let score = match symbol {
+                ')' => 1,
+                ']' => 2,
+                '}' => 3,
+                '>' => 4,
+                _ => 0,
+            };
+
+            acc * 5 + score
+        })
+}
+
+fn invert(symbol: char) -> char {
+    match symbol {
+        '(' => ')',
+        '[' => ']',
+        '{' => '}',
+        '<' => '>',
+        ')' => '(',
+        ']' => '[',
+        '}' => '{',
+        '>' => '<',
+        _ => ' ',
+    }
+}
--- aoc-2021/src/main.rs
+++ aoc-2021/src/main.rs
@@ -9,6 +9,7 @@ mod day06;
 mod day07;
 mod day08;
 mod day09;
+mod day10;
 
 fn main() {
     let day: i32 = env::args().nth(1).expect("day").parse().expect("number");
@@ -23,6 +24,7 @@ fn main() {
         7 => day07::run(),
         8 => day08::run(),
         9 => day09::run(),
+        10 => day10::run(),
         _ => println!("invalid day"),
     }
 }
